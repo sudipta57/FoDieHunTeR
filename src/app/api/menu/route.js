2ba7a842ce9e9f0cbef3712a3c94a7d2f01f1707
sudipta57 <ghoramiswapna32@gmail.com>
2024-03-29 13:53:55 +0530

    trying to fix the data fetching problem

--- src/app/api/menu/route.js
+++ src/app/api/menu/route.js
@@ -1,9 +1,9 @@
 import dbConnect from "@/utils/connection";
 import { NextResponse } from "next/server";
 import foodData from "../../../../models/fooddataschema";
+export const revalidate = 1;
 export async function GET() {
   const connectToDB = await dbConnect();
-
   if (!connectToDB) {
     return NextResponse.json(
       { error: "Error connecting to database" },
